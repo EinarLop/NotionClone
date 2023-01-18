bbcb58590f07529a05ae7aafad4f2649dada77b5
EinarLop <einarlop0@gmail.com>
2023-01-17 22:01:16 -0600

    Headings

--- src/App.js
+++ src/App.js
@@ -1,34 +1,112 @@
 import { useState, createElement, useEffect } from "react";
 
 function App() {
-  let tagCounter = 1;
-
-  const [test, setTest] = useState([
+  let tagCounter = 2;
+  let sequence = "";
+  // p, main title, h1, h2, h3
+  let tags = [
     {
       tag: "input",
       props: {
-        className: "bg-blue-100",
-        onChange: (events) => handleOnChange(events),
+        placeholder: "Begin typing here...",
+        className: "bg-blue-100 w-full focus:outline-none",
+        onKeyDown: (event) => handleKeyDown(event),
+        id: 1,
+      },
+    },
+    {
+      tag: "input",
+      props: {
+        placeholder: "Page Title",
+        className: "bg-blue-100 text-5xl w-full focus:outline-none",
         id: 0,
       },
     },
-  ]);
+    {
+      tag: "input",
+      props: {
+        placeholder: "Page Title",
+        className: "bg-blue-100 text-3xl w-full focus:outline-none",
+        id: 0,
+      },
+    },
+    {
+      tag: "input",
+      props: {
+        placeholder: "Page Title",
+        className: "bg-blue-100 text-2xl w-full focus:outline-none",
+        id: 0,
+      },
+    },
+    {
+      tag: "input",
+      props: {
+        placeholder: "Page Title",
+        className: "bg-blue-100 text-xl w-full focus:outline-none",
+        id: 0,
+      },
+    },
+  ];
 
-  const handleOnChange = async (event) => {
-    if (event.target.value.includes("/p")) {
-      const elementToAdd = {
-        tag: "input",
-        props: {
-          className: "bg-blue-100",
-          onChange: (events) => handleOnChange(events),
-          id: tagCounter,
-        },
-      };
+  const [test, setTest] = useState([tags[1], tags[0]]);
 
-      setTest((test) => [...test, elementToAdd]);
+  const handleKeyDown = (event) => {
+    console.log(sequence);
 
-      event.target.value = event.target.value.replace("/p", "");
-      tagCounter++;
+    if (event.key === "Backspace") {
+      console.log("sssssssssss", event.target.value[1]);
+      if (
+        event.target.value[event.target.value.length - 1] === "p" ||
+        event.target.value[event.target.value.length - 1] === "/" ||
+        event.target.value[event.target.value.length - 1] === "h" ||
+        event.target.value[event.target.value.length - 1] === "1" ||
+        event.target.value[event.target.value.length - 1] === "2"
+      ) {
+        console.log("sss");
+        sequence = sequence.substring(0, sequence.length - 1);
+      }
+    }
+    if (
+      event.key === "p" ||
+      event.key === "/" ||
+      event.key === "h" ||
+      event.key === "1" ||
+      event.key === "2" ||
+      event.key === "3"
+    ) {
+      sequence += event.key;
+    } else if (event.key !== "Enter") {
+      sequence = "";
+    }
+    if (sequence === "/p" && event.key === "Enter") {
+      setTest((test) => [...test, tags[0]]);
+      event.target.value = event.target.value.substring(
+        0,
+        event.target.value.length - 2
+      );
+      sequence = "";
+      console.log("inside");
+    } else if (sequence === "/h1" && event.key === "Enter") {
+      setTest((test) => [...test, tags[2]]);
+      event.target.value = event.target.value.substring(
+        0,
+        event.target.value.length - 2
+      );
+      sequence = "";
+    } else if (sequence === "/h2" && event.key === "Enter") {
+      setTest((test) => [...test, tags[3]]);
+      event.target.value = event.target.value.substring(
+        0,
+        event.target.value.length - 2
+      );
+      sequence = "";
+    } else if (sequence === "/h3" && event.key === "Enter") {
+      setTest((test) => [...test, tags[4]]);
+      event.target.value = event.target.value.substring(
+        0,
+        event.target.value.length - 2
+      );
+      sequence = "";
     }
   };
 
